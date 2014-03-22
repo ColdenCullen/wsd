c438db22b76af28c0c2d908a5dd2456a8a0bd138
Colden Cullen <ColdenCullen@gmail.com>
2014-03-22 16:28:50 -0700

    Removed option for just building

--- source/app.d
+++ source/app.d
@@ -5,23 +5,22 @@ import std.process, std.path, std.string;
 void main( string[] args )
 {
 	// If no args given, print help
-	if( args.length < 3 )
+	if( args.length < 2 )
 	{
-		io.writeln( "Usage: wsd (build|run) (filename) [rdmd options]" );
+		io.writeln( "Usage: wsd (filename) [rdmd options]" );
 		return;
 	}
 
 	// Get name of file to write to
-	auto outFileName = args[2].stripExtension ~ ".d";
+	auto outFileName = args[1].stripExtension ~ ".d";
 
 	// Read file specified
-	auto code = file.readText( args[2] );
+	auto code = file.readText( args[1] );
 	// Translate the code
 	auto translatedCode = processCode( code );
 	// Write resulting code
 	file.write( outFileName, translatedCode );
 
-	// Call rdmd if specified
-	if( args[1].strip == "run" )
-		wait( spawnProcess( [ "rdmd", outFileName ] ~ ( args.length > 3 ? args[ 3..$-1 ] : [] ) ) );
+	// Call rdmd on file
+	wait( spawnProcess( [ "rdmd", outFileName ] ~ ( args.length > 2 ? args[ 2..$-1 ] : [] ) ) );
 }

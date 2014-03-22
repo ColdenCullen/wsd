d425b1e7556951ba9c7c8ddbdcb6e911eecffbe0
Colden Cullen <ColdenCullen@gmail.com>
2014-03-22 16:22:02 -0700

    Added support for build and run options

--- source/app.d
+++ source/app.d
@@ -1,18 +1,27 @@
 import wsd;
-import io = std.stdio, file = std.file, std.path;
+import io = std.stdio, file = std.file;
+import std.process, std.path, std.string;
 
 void main( string[] args )
 {
 	// If no args given, print help
-	if( args.length < 2 )
+	if( args.length < 3 )
 	{
-		io.writeln( "Usage: wsd [filename].wsd [rdmd options]" );
+		io.writeln( "Usage: wsd (build|run) (filename) [rdmd options]" );
 		return;
 	}
 
-	auto outFileName = args[1].dirName ~ args[1].baseName ~ ".d";
+	// Get name of file to write to
+	auto outFileName = args[2].stripExtension ~ ".d";
 
-	auto code = file.readText( args[1] );
+	// Read file specified
+	auto code = file.readText( args[2] );
+	// Translate the code
 	auto translatedCode = processCode( code );
+	// Write resulting code
 	file.write( outFileName, translatedCode );
+
+	// Call rdmd if specified
+	if( args[1].strip == "run" )
+		wait( spawnProcess( [ "rdmd", outFileName ] ~ ( args.length > 3 ? args[ 3..$-1 ] : [] ) ) );
 }
